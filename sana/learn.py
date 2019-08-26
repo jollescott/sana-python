db6da88a29d8fddb9a92c5f199648d69aae0f383
Joel Linder <JoLi0125@student.grillska.se>
2019-08-26 19:58:00 +0200

    Asset management implementation

--- sana/learn.py
+++ sana/learn.py
@@ -1,8 +1,11 @@
 from sana import SANA_API_KEY, SANA_API_REGION
+import requests
+import json
 
 #############
 # Utilities #
 #############
+
 
 def get_base_url():
     if SANA_API_REGION == 'US':
@@ -13,6 +16,7 @@ def get_base_url():
 ####################
 # Asset Management #
 ####################
+
 
 class LearnAsset():
 
@@ -25,23 +29,47 @@ class LearnAsset():
         self.content_url = content_url
         self.metadata = metadata
 
+
 def create_or_update_asset(asset):
-    pass
+    url = '{0}/v1/assets/{1}'.format(get_base_url(), asset.id)
+    r = requests.put(url, asset)
+
+    return r.ok
+
 
 def delete_asset(id):
-    pass
+    url = '{0}/v1/assets/{1}'.format(get_base_url(), id)
+    r = requests.delete(url)
+
+    return r.ok
+
 
 def get_asset(id):
-    pass
+    url = '{0}/v1/assets/{1}'.format(get_base_url(), id)
+    r = requests.get(url)
+
+    return r.json()
+
 
 def create_or_update_assets(assets):
-    pass
+    url = '{0}/v1/assets'.format(get_base_url())
+    r = requests.put(url, assets)
+
+    return r.ok()
+
 
 def delete_assets(ids):
-    pass
+    url = '{0}/v1/assets?asset_ids={1}'.format(get_base_url(), json.dumps(ids))
+    r = requests.delete(url)
+
+    return r.ok
+
 
 def get_assets(ids):
-    pass
+    url = '{0}/v1/assets?asset_ids={1}'.format(get_base_url(), json.dumps(ids))
+    r = requests.get(url)
+
+    return r.json()
 
 
 ####################
