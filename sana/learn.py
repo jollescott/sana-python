8277c5d62f7ef6225fe77fb5a9205369c939f163
Joel Linder <JoLi0125@student.grillska.se>
2019-08-26 20:35:27 +0200

    Added views management

--- sana/learn.py
+++ sana/learn.py
@@ -2,6 +2,7 @@ from sana import SANA_API_KEY, SANA_API_REGION
 import requests
 import json
 
+
 #############
 # Utilities #
 #############
@@ -12,6 +13,7 @@ def get_base_url():
         return 'https://us.api.sanalabs.com'
     else:
         return 'https://api.sanalabs.com'
+
 
 ####################
 # Asset Management #
@@ -76,7 +78,54 @@ def get_assets(ids):
 # Views Management #
 ####################
 
+class LearnView():
+    def __init__(self, name, path='', description='', ordered=False, items):
+        self.name = name
+        self.path = path
+        self.description = description
+        self.ordered = ordered
+        self.items = items
 
+
+def create_or_update_view(id, view):
+    url = '{0}/v1/views/{1}'.format(get_base_url(), id)
+    r = requests.put(url, view)
+
+    return r.ok()
+
+
+def delete_view(id):
+    url = '{0}/v1/views/{1}'.format(get_base_url(), id)
+    r = requests.delete(url)
+
+    return r.ok()
+
+
+def get_view(id):
+    url = '{0}/v1/views/{1}'.format(get_base_url(), id)
+    r = requests.get(url)
+
+    return r.json()
+
+def get_all_views(last_view_id=None,limit=None)
+    url = '{0}/v1/views'.format(get_base_url())
+
+    params = []
+
+    if last_view_id is not None:
+        params.append(('last_view_id',last_view_id))
+
+    if limit is not None:
+        params.append(('limit', limit))
+
+    if len(params) > 0:
+        url = url + '?'
+
+        for param in params:
+            url = url + '&{0}={1}'.format(param[0], param[1])
+
+    r = requests.get(url)
+    return r.json()
 
 ######################
 # Realtime Endpoints #
