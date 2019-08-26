7a836184c1131565e931707e5934af4edfa2c0a9
Joel Linder <JoLi0125@student.grillska.se>
2019-08-26 17:30:43 +0200

    Added travis yml & requests dependencies

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
     setuptools.setup(
-        name="sana-python-joellinder",
+        name="sana-python",
         version="0.0.1",
         author="Joel Linder",
         author_email="business@joellinder.dev",
@@ -13,6 +13,10 @@ with open("README.md", "r") as fh:
         long_description_content_type="text/markdown",
         url="https://github.com/jollescott/sana-python",
         packages=setuptools.find_packages(),
+        license='MIT',
+        install_requires=[
+            'requests',
+        ],
         classifiers=[
             "Programming Language :: Python :: 3",
             "License :: OSI Approved :: MIT License",
